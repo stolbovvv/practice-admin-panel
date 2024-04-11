fd7cbbe31acb1d06d1313ac5eba23ddfa8b6db12
Valerii Stolbov <stolbov.v@outlook.com>
2024-04-11 17:41:35 +0500

    Update URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/practice-react-redux-admin/',
+  base: '/practice-admin-panel/',
   plugins: [react()],
 });
